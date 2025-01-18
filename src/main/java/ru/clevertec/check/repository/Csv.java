f0c83dea11f8d554a53bfa12c6b7a29b1dff2363
mLastovsky <LastovskyTech@gmail.com>
2025-01-19 02:46:45 +0300

    add: create Csv interface

---
+++ src/main/java/ru/clevertec/check/repository/Csv.java
@@ -0,0 +1,23 @@
+package main.java.ru.clevertec.check.repository;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static java.nio.file.StandardOpenOption.*;
+
+public interface Csv {
+
+    int HEADER_ROW_INDEX = 1;
+
+    default List<String> readCsvFile(Path path) throws IOException {
+        return Files.readAllLines(path).stream()
+                .skip(HEADER_ROW_INDEX)
+                .toList();
+    }
+
+    default void writeCsvFile(Path path, List<String> rows) throws IOException {
+        Files.write(path, rows, CREATE, TRUNCATE_EXISTING);
+    }
+}
