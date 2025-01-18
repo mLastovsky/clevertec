679f3ae911bf57d9e9a08c35e442b75094246472
mLastovsky <LastovskyTech@gmail.com>
2025-01-18 09:44:57 +0300

    impl: ProductQuantityMarshaller

---
+++ src/main/java/ru/clevertec/check/util/argument/marshaler/ProductQuantityMarshaler.java
@@ -0,0 +1,22 @@
+package main.java.ru.clevertec.check.util.argument.marshaler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductQuantityMarshaler implements ArgumentMarshaler{
+
+    private Map<Integer, Integer> idQuantity = new HashMap<>();
+
+    @Override
+    public void addValue(Object... values) {
+        int productId = (Integer) values[0];
+        int quantity = (Integer) values[1];
+
+        idQuantity.merge(productId, quantity, Integer::sum);
+    }
+
+    @Override
+    public Object getArgumentValue() {
+        return idQuantity;
+    }
+}
