d5195cfef8433d3a5840bd00ef06ac0f7d964846
mLastovsky <LastovskyTech@gmail.com>
2025-01-18 20:36:59 +0300

    impl: implement Repository interface for various Entities

---
+++ src/main/java/ru/clevertec/check/repository/DiscountCardRepository.java
@@ -0,0 +1,7 @@
+package main.java.ru.clevertec.check.repository;
+
+import main.java.ru.clevertec.check.model.DiscountCard;
+
+public interface DiscountCardRepository extends Repository<DiscountCard, Long> {
+
+}
---
+++ src/main/java/ru/clevertec/check/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package main.java.ru.clevertec.check.repository;
+
+import main.java.ru.clevertec.check.model.Product;
+
+public interface ProductRepository extends Repository<Product, Long> {
+
+}
