2d8b1533bdf6e5a3f836358371c541a2e41eec23
mLastovsky <LastovskyTech@gmail.com>
2025-01-19 03:45:14 +0300

    feat(DiscountCardRepository): add method to find Card by number

--- src/main/java/ru/clevertec/check/repository/DiscountCardRepository.java
+++ src/main/java/ru/clevertec/check/repository/DiscountCardRepository.java
@@ -2,6 +2,10 @@ package main.java.ru.clevertec.check.repository;
 
 import main.java.ru.clevertec.check.model.DiscountCard;
 
+import java.util.Optional;
+
 public interface DiscountCardRepository extends Repository<DiscountCard, Long> {
+
+    Optional<DiscountCard> findByNumber(String cardNumber);
 
 }
