51bcb02ce30f20a0ae707c2232c657f98a70fd2d
mLastovsky <LastovskyTech@gmail.com>
2025-01-19 19:25:24 +0300

    refactor(CsvMapper): extends Csv interface instead of create Separator field

--- src/main/java/ru/clevertec/check/mapper/csv/CsvMapper.java
+++ src/main/java/ru/clevertec/check/mapper/csv/CsvMapper.java
@@ -1,9 +1,8 @@
 package main.java.ru.clevertec.check.mapper.csv;
 
 import main.java.ru.clevertec.check.mapper.Mapper;
+import main.java.ru.clevertec.check.repository.csv.Csv;
 
-public interface CsvMapper<F, T> extends Mapper<F, T> {
-
-    String COLUMN_SEPARATOR = ";";
+public interface CsvMapper<F, T> extends Csv, Mapper<F, T> {
 
 }
