4a3c304d4cdabe10836d052e2646f38b9dc888cc
mLastovsky <LastovskyTech@gmail.com>
2025-01-21 15:14:13 +0300

    feat(ProductService): feat: add wholesale discount calculation logic with eligibility check

--- src/main/java/ru/clevertec/check/service/ProductService.java
+++ src/main/java/ru/clevertec/check/service/ProductService.java
@@ -4,9 +4,13 @@ import main.java.ru.clevertec.check.exception.BadRequestException;
 import main.java.ru.clevertec.check.model.Product;
 import main.java.ru.clevertec.check.repository.ProductRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class ProductService {
+
+    private static final int WHOLESALE_THRESHOLD = 5;
+    private static final BigDecimal WHOLESALE_DISCOUNT_PERCENT = BigDecimal.valueOf(10);
 
     private final ProductRepository productRepository;
 
@@ -23,5 +27,17 @@ public class ProductService {
                 .orElseThrow(
                         () -> new BadRequestException("Product not found")
                 );
+    }
+
+    public BigDecimal calculateWholesaleDiscount(Product product, Integer quantity){
+        if (isEligibleForWholesaleDiscount(product, quantity)) {
+            var totalPrice = product.getPrice().multiply(BigDecimal.valueOf(quantity));
+            return totalPrice.multiply(WHOLESALE_DISCOUNT_PERCENT).divide(BigDecimal.valueOf(100));
+        }
+        return BigDecimal.ZERO;
+    }
+
+    public boolean isEligibleForWholesaleDiscount(Product product, Integer quantity) {
+        return product.isWholesaleProduct() && quantity >= WHOLESALE_THRESHOLD;
     }
 }
