fbf56539693e111b74609243b558169236b1cf9f
mLastovsky <LastovskyTech@gmail.com>
2025-01-24 06:27:35 +0300

    add: create Marshalers to storage PathToFilePath and SaveToFilePath

---
+++ src/main/java/ru/clevertec/check/parser/argument/marshaler/SaveToFileMarshaler.java
@@ -0,0 +1,14 @@
+package main.java.ru.clevertec.check.parser.argument.marshaler;
+
+public class SaveToFileMarshaler implements ArgumentMarshaler {
+
+    @Override
+    public void addValue(Object... values) {
+
+    }
+
+    @Override
+    public Object getArgumentValue() {
+        return null;
+    }
+}
