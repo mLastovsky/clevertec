723e8a6e8af7e126b75f223a5351a4fe78892d03
mLastovsky <LastovskyTech@gmail.com>
2025-01-24 03:08:15 +0300

    feat(CheckService): added checking the card for null

--- src/main/java/ru/clevertec/check/service/CheckService.java
+++ src/main/java/ru/clevertec/check/service/CheckService.java
@@ -1,6 +1,7 @@
 package main.java.ru.clevertec.check.service;
 
 import main.java.ru.clevertec.check.exception.BadRequestException;
+import main.java.ru.clevertec.check.exception.InternalServerError;
 import main.java.ru.clevertec.check.model.Check;
 import main.java.ru.clevertec.check.model.CheckItem;
 import main.java.ru.clevertec.check.model.DiscountCard;
@@ -29,12 +30,12 @@ public class CheckService {
         this.checkSenderProxy = checkSenderProxy;
     }
 
-    public void generateReports(String[] args) throws BadRequestException {
+    public void generateReports(String[] args) throws BadRequestException, InternalServerError {
         var check = createCheck(args);
         checkSenderProxy.send(check);
     }
 
-    public Check createCheck(String[] args) throws BadRequestException {
+    public Check createCheck(String[] args) throws BadRequestException, InternalServerError {
         var argsParser = new ArgsParser(args);
         var discountCardNumber = (Integer) argsParser.getArgumentValueByNameOrDefault("discountCard", null);
         var productQuantities = (List<ProductQuantity>) argsParser.getArgumentValueByNameOrDefault("productQuantities", List.of());
@@ -62,7 +63,7 @@ public class CheckService {
                 .build();
     }
 
-    private List<CheckItem> convertToCheckItems(List<ProductQuantity> productQuantities, DiscountCard discountCard) throws BadRequestException {
+    private List<CheckItem> convertToCheckItems(List<ProductQuantity> productQuantities, DiscountCard discountCard) throws BadRequestException, InternalServerError {
         List<CheckItem> checkItems = new ArrayList<>();
         for (var element : productQuantities) {
             checkItems.add(convertToCheckItem(element, discountCard));
@@ -70,10 +71,17 @@ public class CheckService {
         return checkItems;
     }
 
-    private CheckItem convertToCheckItem(ProductQuantity productQuantity, DiscountCard discountCard) throws BadRequestException {
+    private CheckItem convertToCheckItem(ProductQuantity productQuantity, DiscountCard discountCard) throws BadRequestException, InternalServerError {
         var productId = productQuantity.getId();
         var quantity = productQuantity.getQuantity();
         var product = productService.getProductById(productId);
+
+        if(product.getQuantityInStock() < quantity){
+            throw new InternalServerError(
+                    String.format("Insufficient stock for product ID %d. Requested: %d, Available: %d",
+                    productId, quantity, product.getQuantityInStock()));
+        }
+
         var totalDiscount = calculateDiscountsForCheckItem(product, quantity, discountCard);
         var totalPrice = calculateTotalPriceForCheckItem(product, quantity);
 
@@ -93,7 +101,11 @@ public class CheckService {
             return wholesaleDiscount;
         }
 
-        return discountCardService.calculateCardDiscount(product, quantity, discountCard);
+        if(discountCard != null){
+            return discountCardService.calculateCardDiscount(product, quantity, discountCard);
+        }
+
+        return BigDecimal.ZERO;
     }
 
 
