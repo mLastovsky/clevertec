1a18578e7ddc617b744f0f966bc92920bafe00f3
mLastovsky <LastovskyTech@gmail.com>
2025-01-27 02:05:50 +0300

    feat(PathToFileMarshaler): release addValue method

--- src/main/java/ru/clevertec/check/parser/argument/marshaler/PathToFileMarshaler.java
+++ src/main/java/ru/clevertec/check/parser/argument/marshaler/PathToFileMarshaler.java
@@ -2,13 +2,13 @@ package main.java.ru.clevertec.check.parser.argument.marshaler;
 
 import java.nio.file.Path;
 
-public class PathToFileMarshaler implements ArgumentMarshaler{
+public class PathToFileMarshaler implements ArgumentMarshaler {
 
     private Path pathToFile;
 
     @Override
     public void addValue(Object... values) {
-        pathToFile = (Path) values[0];
+        pathToFile = Path.of((String) values[0]);
     }
 
     @Override
